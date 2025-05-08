ae8a79eaecc124325ed7357e3ad467e00994f120
tlssunw <sunw1044@gmail.com>
2025-05-08 11:21:48 +0900

    5월 8일 신선우

--- src/main/java/com/recipe/config/SecurityConfig.java
+++ src/main/java/com/recipe/config/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.formLogin(Customizer.withDefaults());
+        // http.formLogin(Customizer.withDefaults());
         return http.build();
     }
 }
--- src/main/java/com/recipe/control/MainController.java
+++ src/main/java/com/recipe/control/MainController.java
@@ -15,35 +15,20 @@ public class MainController {
     @GetMapping("/")
     public String mainpage(Model model){
 
-//        User user = new User();
-//        user.setNickName("신선우");
-//        user.setPassword("1234");
-//
-//        User user2 = new User();
-//        user2.setNickName("신선우2");
-//        user2.setPassword("12345");
-//
-//        User user3 = new User();
-//        user3.setNickName("신선우3");
-//        user3.setPassword("123456");
-//
-//        List<User> userList = new ArrayList<>();
-//        userList.add(user);
-//        userList.add(user2);
-//        userList.add(user3);
-//
-//        // 객체에 데이터를 넣은 다음에 모델에 넣어 넘김
-//       User user = new User();
-//       user.setNickName("신선우");
-//       user.setPassword("1234");
-
         model.addAttribute("userInfo", userService.getAllUsers() );
         // 모델에 데이터를 담아(에드에트리뷰트라는 메서드를 이용해서) - 이름을 정해서
         // 이름은 문자열 타입으로~~
         return "mainpage";
     }
 
-    // get post
-    // get - 대놓고 줌(빠르게확인)
-    // post - 전달할 때 봉투에담아서줌(느리게확인)
+    @GetMapping("/login")
+    public String login(Model model){
+       return "user/login";
+    }
+
+    @GetMapping("/signup")
+    public String signup(Model model){
+        return "user/signup";
+    }
+
 }
